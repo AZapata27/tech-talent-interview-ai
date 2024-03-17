f2ce6a4021d4451a533c6b82847c2bd617a87c8f
Brayan <brayan.erazo@quind.io>
2024-03-17 12:06:31 -0500

    Configuracion inicial del proyecto

---
+++ src/main/java/co/com/flypass/backendarqueotipo/BackendArqueotipoApplication.java
@@ -0,0 +1,13 @@
+package co.com.flypass.backendarqueotipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendArqueotipoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendArqueotipoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/flypass/backendarqueotipo/BackendArqueotipoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.flypass.backendarqueotipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendArqueotipoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
