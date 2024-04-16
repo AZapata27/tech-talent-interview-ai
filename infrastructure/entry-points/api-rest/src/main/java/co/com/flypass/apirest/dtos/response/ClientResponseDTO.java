9541d80576a8fc156c1d9cf9f26a3b9b921ec052
Brayan <brayan.erazo@quind.io>
2024-04-15 22:49:13 -0500

    Se modifica la base de la url y se agrega metodos toString, equals y hascode al ClientResponseDTO

--- infrastructure/entry-points/api-rest/src/main/java/co/com/flypass/apirest/dtos/response/ClientResponseDTO.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/flypass/apirest/dtos/response/ClientResponseDTO.java
@@ -1,6 +1,8 @@
 package co.com.flypass.apirest.dtos.response;
 
 
+import java.util.Objects;
+
 public class ClientResponseDTO {
     private Long id;
     private String identificationType;
@@ -82,5 +84,32 @@ public class ClientResponseDTO {
 
     public void setModificationDate(String modificationDate) {
         this.modificationDate = modificationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientResponseDTO{" +
+                "dateOfBirth='" + dateOfBirth + '\'' +
+                ", creationDate='" + creationDate + '\'' +
+                ", modificationDate='" + modificationDate + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", identificationNumber=" + identificationNumber +
+                ", identificationType='" + identificationType + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ClientResponseDTO that)) return false;
+        return Objects.equals(id, that.id) && Objects.equals(identificationType, that.identificationType) && Objects.equals(identificationNumber, that.identificationNumber) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(emailAddress, that.emailAddress) && Objects.equals(dateOfBirth, that.dateOfBirth) && Objects.equals(creationDate, that.creationDate) && Objects.equals(modificationDate, that.modificationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, identificationType, identificationNumber, firstName, lastName, emailAddress, dateOfBirth, creationDate, modificationDate);
     }
 }
